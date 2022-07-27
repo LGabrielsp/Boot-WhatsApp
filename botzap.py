c6cfa84066aa4e6e6136e98c4248da0d3d6d9114
Luis_Gabriel <luis.g.s.pallazzi@gmail.com>
2022-07-26 23:10:43 -0300

    Ajustes

--- botzap.py
+++ botzap.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import time
 
 class WhatsappBot:
-    def _init_(self):
+    def __init__(self):
 	    # Mensagem que será enviada
         self.mensagem = "Bom dia, tudo de bom hoje"
 	    # Grupos para que será enviada
@@ -12,22 +12,21 @@ class WhatsappBot:
         self.driver = webdriver.Chrome(executable_path=r'./chromedriver.exe', chrome_options=options)
 
     def EnviarMensagens(self):
+        print(self.driver)
         self.driver.get('https://web.whatsapp.com/')
-        time.spleep(30)
+        time.sleep(30)
         for grupo in self.grupos:
-            campo_grupo = self.driver.find_element_by_xpath(f"//span[@title='{grupo}']")
+            grupo = self.driver.find_element_by_xpath(f"//span[@title='{grupo}']")
             time.spleep(3)
-            campo_grupo.click()
+            grupo.click()
             chat_box = self.driver.find_element_by_class_name('_p3_M1')
             time.sleep(3)
             chat_box.click()
             chat_box.send_keys(self.mensagem)
-            # <span data-testid="send" data-icon="send" class="">
             botao_enviar = self.driver.find_element_by_xpath("//span[@data-icon='send']")
             time.sleep(3)
             botao_enviar.click()
             time.sleep(3)
-
 
 bot = WhatsappBot()
 bot.EnviarMensagens()
